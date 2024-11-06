497da3fc62e3ca18aedc2c1cc3e9f9efdd67a050
שלי דהן <slydhn@h-MacBook-Air-sl-sly.local>
2024-11-06 15:26:09 +0200

    numwords2

--- NumWords.java
+++ NumWords.java
@@ -6,6 +6,9 @@ public class NumWords {
 		int o = number1 % 10;
 		int t = ((number1 % 100)- o)/10 ;
 		int h = ((number1%1000)-o -t)/100;
+		if(number1 > 999){
+			h = number1/100;
+		}
 		System.out.println( h + " hundreds, " + t + " tens, " + "and " + o + " ones.");
 	}
 }
