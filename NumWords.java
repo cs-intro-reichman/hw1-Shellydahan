fda4696613a8ab06ef06f606e0ee3524dbac1f5c
שלי דהן <slydhn@h-MacBook-Air-sl-sly.local>
2024-11-06 15:14:08 +0200

    numwords1

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,10 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int number1 = Integer.parseInt(args[0]);
+		int o = number1 % 10;
+		int t = ((number1 % 100)- o)/10 ;
+		int h = ((number1%1000)-o -t)/100;
+		System.out.println( h + " hundreds, " + t + " tens, " + "and " + o + " ones.");
 	}
 }
